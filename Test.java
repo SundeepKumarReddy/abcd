cfa0607b0130af0db3e455e9832c8fb63806ee83
SundeepKumarReddy <86119004+SundeepKumarReddy@users.noreply.github.com>
2021-06-18 21:30:42 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+class Test
+{
+  int a=9;
+  System.out.println("The val of a = "+a);
+  /*Test()
+  {
+   System.out.println("The val of a = "+a);
+   System.out.println("The val of = "+b);
+   a=10;
+   b=20;
+   System.out.println("The val of a = "+a);
+   System.out.println("The val of b = "+b);
+  }*/
+}  
+  
